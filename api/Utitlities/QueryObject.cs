686f07827f8d3110c67eff68129943597bf08099
rrapikola <rrapikola@aol.com>
2024-02-24 16:27:36 +0100

    Adding Query

---
+++ api/Utitlities/QueryObject.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Utitlities
+{
+    public class QueryObject
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        
+    }
+}
